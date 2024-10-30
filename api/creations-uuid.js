2eb1c600e60e7cc2ccd7803cb589142004e56ea6
zjusticy <stevens123321@outlook.com>
2024-10-30 15:42:58 -0700

    quick fix

--- api/creations-uuid.js
+++ api/creations-uuid.js
@@ -54,7 +54,7 @@ export default async function handler(req, res) {
     // }
     const data = await response.json();
 
-    res.setHeader('Access-Control-Allow-Origin', '');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
