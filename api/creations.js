d736405cb32ad9c633cd28812d01be27ba1e2b7f
zjusticy <stevens123321@outlook.com>
2024-10-30 15:29:27 -0700

    try 5

--- api/creations.js
+++ api/creations.js
@@ -86,11 +86,11 @@ export default async function handler(req, res) {
     // }
     const data = await response.json();
 
-    return res.status(200).json(data, {
-      headers: {
-        ...corsHeaders,
-      },
-    });
+    res.setHeader('Access-Control-Allow-Origin', '');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    return res.status(200).json(data);
   } catch (error) {
     console.log(error);
   }
